d862d7863eae3dfb4252a621831b6478f36b87fb
sayedhassan-husssaini <sh.hussaini1378@gmail.com>
2022-04-10 11:51:00 +0430

    set design od login page

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: (credentials) => {
+        // authroize user
+      },
+    }),
+  ],
+});
