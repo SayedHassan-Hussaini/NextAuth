ffadf3665c331dc1acc6dc8a0afef2762be5ae22
sayedhassan-husssaini <sh.hussaini1378@gmail.com>
2022-04-10 13:29:20 +0430

    fix api for login

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,58 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { signIn } from "next-auth/react";
 
 export default function Home() {
+  async function handleLogin(e) {
+    e.preventDefault();
+    const formElement = document.getElementById("login");
+    const form = new FormData(formElement);
+    console.log("form", form.get("email"), form.get("pass"));
+    const req = await signIn("credentials", {
+      email: form.get("email"),
+      password: form.get("pass"),
+      redirect: false,
+    });
+    console.log("res....", await req.json());
+  }
   return (
     <>
-     <h2>Login Page</h2><br/>    
-    <div class="login">    
-    <form id="login" method="get" action="login.php">    
-          <label><b>User Name</b></label>    
-          <input type="text" name="Uname" id="Uname" placeholder="Username"/>    
-          <br/><br/>    
-          <label><b>Password</b>
-          <input type="Password" name="Pass" id="Pass" placeholder="Password"/>        
-          </label> 
-          <br/><br/>     
-          <input type="checkbox" id="check"/>    
-          <span>Remember me</span>    
-          <br/><br/>   
-          <input type="button" name="log" id="log" value="Log In Here"/>
-          <br/><br/> 
-           <a href="#">Forgot Password</a>    
-      </form>    
-    </div>
+      <h2>Login Page</h2>
+      <br />
+      <div className="login">
+        <form id="login" method="post" onSubmit={handleLogin}>
+          <label>
+            <b>User Name</b>
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="Uname"
+            placeholder="test@gmail"
+          />
+          <br />
+          <br />
+          <label>
+            <b>Password</b>
+            <input
+              type="Password"
+              name="pass"
+              id="Pass"
+              placeholder="Password"
+            />
+          </label>
+          <br />
+          <br />
+          <input type="checkbox" id="check" />
+          <span>Remember me</span>
+          <br />
+          <br />
+          <button type="submit" name="log" id="log">
+            Log In Here
+          </button>
+          <br />
+          <br />
+          <a href="#">Forgot Password</a>
+        </form>
+      </div>
     </>
-  )
+  );
 }
