9e9ccedebb3f7287e599c5278761a0ddd9980f45
sayedhassan-husssaini <sh.hussaini1378@gmail.com>
2022-04-11 10:01:57 +0430

    fix error

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
-import { useState } from "react";
+import {useState} from 'react'
 import { signIn, getSession } from "next-auth/react";
-import { useRouter } from "next/router";
-import { Formik, Form } from "formik";
-import Yap from  'Yap'
+import {useRouter} from 'next/router'
 
-function Home({ data }) {
-  const router = useRouter();
-  const [error, setError] = useState(false);
+function Home({data}) {
+
+  const router=useRouter()
+  const [error,setError]=useState(false)
 
   async function handleLogin(e) {
     e.preventDefault();
@@ -17,67 +16,56 @@ function Home({ data }) {
       password: form.get("pass"),
       redirect: false,
     });
-    if (req.status == 200) {
-      router.push("/dashboard");
-    } else {
-      setError(true);
+    if(req.status==200){
+      router.push('/dashboard')
+    }else{
+      setError(true)
     }
   }
 
+  
   return (
     <>
       <h2>Login Page</h2>
       <br />
       <div className="login">
-        <Formik
-          initialValues={{
-            email: "test@gmail.com",
-            pass: "1234",
-          }}
-          
-        >
-          {(formik) => {
-            <Form id="login" onSubmit={handleLogin}>
-              <label>
-                <b>User Name</b>
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="Uname"
-                placeholder="test@gmail"
-              />
-              <br />
-              <br />
-              <label>
-                <b>Password</b>
-                <input
-                  type="Password"
-                  name="pass"
-                  id="Pass"
-                  placeholder="1234"
-                />
-              </label>
-              <br />
-              <br />
-              <input type="checkbox" id="check" />
-              <span>Remember me</span>
-              <br />
-              <br />
-              <button type="submit" name="log" id="log">
-                Log In Here
-              </button>
-              <br />
-              <br />
-              <a href="#">Forgot Password</a>
-              {error ? (
-                <div className="error">Invaled Email or Password.</div>
-              ) : (
-                ""
-              )}
-            </Form>;
-          }}
-        </Formik>
+        <form id="login" method="post" onSubmit={handleLogin}>
+          <label>
+            <b>User Name</b>
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="Uname"
+            placeholder="test@gmail"
+          />
+          <br />
+          <br />
+          <label>
+            <b>Password</b>
+            <input
+              type="Password"
+              name="pass"
+              id="Pass"
+              placeholder="1234"
+            />
+          </label>
+          <br />
+          <br />
+          <input type="checkbox" id="check" />
+          <span>Remember me</span>
+          <br />
+          <br />
+          <button type="submit" name="log" id="log">
+            Log In Here
+          </button>
+          <br />
+          <br />
+          <a href="#">Forgot Password</a>
+          {error?<div className='error'>
+            Invaled Email or Password.
+          </div>:""}
+        </form>
       </div>
     </>
   );
